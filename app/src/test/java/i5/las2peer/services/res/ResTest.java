74ba7f6066a5c841cc39c0d9555f49ef46e68450
PhilCAEUser <mail@rwth>
2022-07-13 09:15:08 +0000

    add test case

--- app/src/test/java/i5/las2peer/services/res/ResTest.java
+++ app/src/test/java/i5/las2peer/services/res/ResTest.java
@@ -116,6 +116,33 @@ public class ResTest {
   }
 
 
+  /**
+   * 
+   * Test for the NewTestCase_ID620791 method.
+   * 
+   */
+  @Test
+  public void testNewTestCase_ID620791() {
+    MiniClientCoverage c = new MiniClientCoverage(mainPath);
+    c.setConnectorEndpoint(connector.getHttpEndpoint());
+    
+        
+    try {
+      c.setLogin(AnonymousAgentImpl.IDENTIFIER, "");
+      ClientResponse result = c.sendRequest("GET", "/tesat", """
+""", "text/plain", "*/*", new HashMap<>(), new Object[0]);
+      System.out.println("Result of request with id: 385299: " + result.getResponse().trim());
+    
+      Assert.assertEquals("[462416]", 200, result.getHttpCode());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Exception: " + e);
+    }
+    
+
+    
+  }
 
 
 
